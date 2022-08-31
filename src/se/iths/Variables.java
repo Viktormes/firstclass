74e94bf6523b94f5bc8893aa013a14fa075efdf9
Viktor <vikhag0722@gmail.com>
2022-08-31 13:14:32 +0200

    Initial commit

---
+++ src/se/iths/Variables.java
@@ -0,0 +1,23 @@
+package se.iths;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Variables {
+    public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+        System.out.print("Hej, vad heter du? :");
+        String hej = scanner.next();
+        System.out.println("Hej " + hej + " var god skriv en siffra.");
+        int siffra = scanner.nextInt();
+        System.out.print("Du valde siffran: " + siffra );
+
+
+
+
+        }
+
+
+    }
+
---
+++ src/se/iths/firstclass.java
@@ -0,0 +1,15 @@
+package se.iths;
+
+public class firstclass {
+
+    public static void main(String[] args) {
+
+        for (int i = 10; i <=100 ; i += 10) {
+            System.out.println(i);
+
+        }
+
+
+
+    }
+}
